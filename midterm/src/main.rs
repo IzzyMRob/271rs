7f5ba469ad474f8a24c3a072dd8aafc558291864
IzzyMRob <imrobbins@willamette.edu>
2025-10-07 19:55:11 -0700

    working

--- midterm/src/main.rs
+++ midterm/src/main.rs
@@ -9,12 +9,45 @@ fn main() {
     let mut out_file = File::create("out.txt").unwrap();
     let mut in_file = File::open(filename.trim()).unwrap();
     
+    let long_bytes = in_file.bytes();
+    'main: loop {
+        let f = long_bytes.next().map(|v| v.unwrap());
+        let s = long_bytes.next().map(|v| v.unwrap());
+        let t = long_bytes.next().map(|v| v.unwrap());
+        match () {
+            (Some(f), Some(s), Some(t)) => {
+                let s_one = f & 0b00111111;
+                let s_two = f >> 6 | s << 2;
+                let s_three = s << 4 | t >> 6;
+                let s_four = t & 0b11111100;
+                continue 'main;
+            }
+            (Some(f), Some(s), None) => {
+                let s_one = f & 0b00111111;
+                let s_two = f >> 6 | s << 2;
+                let s_three = s >> 4
+                let s_four = 
+                break 'main
+            }
+            (Some(f), None, None) => {
+                let s_one = f & 0b00111111;
+                let s_two = f >> 6;
+                let s_three = 
+                let s_four =
+                break 'main;
+            }
+            (None,None,None) =>{
+                break 'main;
+            } 
+            _ => unreachable!(),
+        }
+     }
 
 }
 
 
 fn num_to_char(n : u8) -> char {
-    latters = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    letters = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     match n {
         0..52 => letters[n as usize] as char,
         52..62 => (n - 52) as char,
